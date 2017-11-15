5c0a96d4d668b7e2c0236d0adbe14fd60692f3c7
valenciaw <32558440+valenciaw@users.noreply.github.com>
2017-11-14 16:39:29 -0800

    Add files via upload

---
+++ DACA project/script.js
@@ -0,0 +1,18 @@
+function myFunction(){
+	document.getElementById("demo").innerHTML = "   ";
+}
+
+
+function myFunction2(){
+	document.getElementById("demo").innerHTML = "In 2017, the amount of people at risk of losing DACA is close to the amount of minutes in a year."
+}
+
+
+function myFunction3(){
+	document.getElementById("demo").innerHTML = "In 2018, the number of people who will lose DACA is about equal to the number of teachers employed in California";
+}
+
+
+function myFunction4(){
+	document.getElementById("demo").innerHTML = "In 2019, the number of people who will have lost DACA is about the number of Apple computers sold in a month";
+}
